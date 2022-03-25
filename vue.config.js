689aa2b363021cc9f7dc44dc5123373616b01a4c
Boiko Daria <79093855+boikodaria22@users.noreply.github.com>
2022-03-25 19:15:40 +0200

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "devServer": {
+      "public": "51.158.96.149",
+      "disableHostCheck": true
+    },
+    // outputDir: "/usr/share/nginx/vue/rockband-landing",
+    // publicPath: process.env.NODE_ENV === 'production'
+    //   ? '/'
+    //   : '/dev/'
+  }
